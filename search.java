fee6bd4490d849835bae19b4d05ef0c445e9ba2f
Aditya mohite <62967416+aditya1318@users.noreply.github.com>
2020-10-01 01:39:20 +0530

    Update search.java

--- search.java
+++ search.java
@@ -3,8 +3,12 @@ public Node search(Node root, int key)
 { 
 	// Base Cases: root is null or key is present at root 
 	if (root==null || root.key==key) 
-		return root;  
+		return root; 
+
+	// val is greater than root's key 
+	if (root.key > key) 
+		return search(root.left, key); 
 
 	// val is less than root's key 
 	return search(root.right, key); 
-} 
+}
